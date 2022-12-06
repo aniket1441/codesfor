4b119294868cbc2e1b86211fd3cc2a683268a419
Adjain15 <74244366+Adjain15@users.noreply.github.com>
2022-12-06 07:52:53 +0530

    Update KMP_Algo_updated.cpp

--- KMP_Algo_updated.cpp
+++ KMP_Algo_updated.cpp
@@ -1,54 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> prefix_function(string ptr) {
-    int m = ptr.size();
-    vector<int> pi(m, 0);
-    for (int i = 1; i < m; i++) {
-        int j = pi[i - 1];
-        while (j > 0 && ptr[i] != ptr[j]) {
-            j = pi[j - 1];
-        }
-        if (ptr[i] == ptr[j]) j++;
+void computeLPSArray(char *pat, int M, int *lps);
 
-        ptr[i] = j;
-    }
-    // time complexity  O(m)
-    return pi;
+void KMPSearch(char *pat, char *txt)
+{
+	int M = strlen(pat);
+	int N = strlen(txt);
+
+	int lps[M];
+
+	computeLPSArray(pat, M, lps);
+
+	int i = 0;
+	int j = 0;
+	while ((N - i) >= (M - j))
+	{
+		if (pat[j] == txt[i])
+		{
+			j++;
+			i++;
+		}
+
+		if (j == M)
+		{
+			printf("Found pattern at index %d ", i - j);
+			j = lps[j - 1];
+		}
+
+		else if (i < N && pat[j] != txt[i])
+		{
+			if (j != 0)
+				j = lps[j - 1];
+			else
+				i = i + 1;
+		}
+	}
 }
 
-void KMPStringSearch(string txt, string ptr) {
-    int M = ptr.size();
-    int N = txt.size();
+void computeLPSArray(char *pat, int M, int *lps)
+{
 
-    vector<int> pi = prefix_function(ptr);
+	int len = 0;
 
-    int i = 0;
-    int j = 0;
-    while (i < N) {
-        if (txt[i] == ptr[j]) {
-            i++;
-            j++;
-        } else {
-            if (j != 0) {
-                j = pi[j - 1];
-            } else {
-                i++;
-            }
-        }
-        if (j == M) {
-            cout << "Pattern found with shift " << i - M << "\n";
-            j = pi[j - 1];
-        }
-    }
+	lps[0] = 0;
+
+	int i = 1;
+	while (i < M)
+	{
+		if (pat[i] == pat[len])
+		{
+			len++;
+			lps[i] = len;
+			i++;
+		}
+		else
+		{
+			if (len != 0)
+			{
+				len = lps[len - 1];
+			}
+			else
+			{
+				lps[i] = 0;
+				i++;
+			}
+		}
+	}
 }
 
-int main() {
-    string text;
-    cin >> text;
-    string pattern;
-    cin >> pattern;
-    KMPStringSearch(text, pattern);
-
-    return 0;
+int main()
+{
+	char txt[] = "ABABDABACDABABCABAB";
+	char pat[] = "ABABCABAB";
+	KMPSearch(pat, txt);
+	return 0;
 }
